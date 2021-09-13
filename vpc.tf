bbde55ef9182977fad8e7a784b6a78a75b494e9f
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-09-13 13:01:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,14 @@
 provider "aws"{
+    region="eu-west-1"
 
+}
+
+terraform {
+  backend "s3" {
+    bucket = "myvpc"
+    key    = "vpc/test.tfstate"
+    region = "us-west-1"
+  }
 }
 
 resource "aws_vpc" "eht" {
@@ -9,3 +18,4 @@ resource "aws_vpc" "eht" {
         Name = "eht"
     }
 }
+
