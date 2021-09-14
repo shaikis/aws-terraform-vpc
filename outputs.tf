f20bb8c2e8bb8a98ad3d7660e4dd182139f425dc
Ismails <shaik.urs@gmail.com>
2021-09-14 08:54:24 +0530

    added output variables

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "vpc_id" {
+    value = aws_vpc.eht.id
+}
 
+output "private_subnet_id"{
+    value = aws_subnet.eht_private_subnet.id
+}
+
+output "pub_subnet_id" {
+    value = aws_subnet.eht_pub_subnet.id
+}
